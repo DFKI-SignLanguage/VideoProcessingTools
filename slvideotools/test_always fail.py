cc479cb7d2f1230b5d91cbee8e02dbadc905f252
CRAYFISH CONGLOMERATE <89016437+crayfisch@users.noreply.github.com>
2022-01-31 15:11:06 +0100

    always failing test added

---
+++ slvideotools/test_always fail.py
@@ -0,0 +1,8 @@
+#
+# Tests to check that the command line interface commands are at least present and loading all the libraries
+import os
+
+
+def test_cli_extract_face_bounds():
+    exit_status = 1
+    assert exit_status == 0
