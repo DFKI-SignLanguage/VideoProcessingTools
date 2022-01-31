1cfe3bffdd7b1a6d5f198b3fb662d9d47b3b31fc
CRAYFISH CONGLOMERATE <89016437+crayfisch@users.noreply.github.com>
2022-01-31 16:02:19 +0100

    recreate test_cli
    Github did a move instead of a copy, so this was accidentallz deleted.

---
+++ slvideotools/test_cli.py
@@ -0,0 +1,23 @@
+# Tests to check that the command line interface commands are at least present and loading all the libraries
+import os
+
+
+def test_cli_extract_face_bounds():
+    exit_status = os.system('python -m slvideotools.extract_face_bounds --help')
+    assert exit_status == 0
+
+
+def test_cli_crop_video():
+    exit_status = os.system('python -m slvideotools.crop_video --help')
+    assert exit_status == 0
+
+
+def test_cli_extract_face_data():
+    exit_status = os.system('python -m slvideotools.extract_face_data --help')
+    assert exit_status == 0
+
+
+def test_cli_trim_video():
+    exit_status = os.system('python -m slvideotools.trim_video --help')
+    assert exit_status == 0
+
